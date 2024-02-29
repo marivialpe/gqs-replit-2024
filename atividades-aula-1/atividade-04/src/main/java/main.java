87fdf0a5cb13e5609e94508546469abcd3fb7464
Mariana Vilela <134511083+marivialpe@users.noreply.github.com>
2024-02-29 14:32:51 +0000

    atividade 04

---
+++ atividades-aula-1/atividade-04/src/main/java/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner console = new Scanner(System.in);
+
+    System.out.print("Informe o cateto a: ");
+    double a = console.nextDouble();
+
+    System.out.print("Informe o cateto b: ");
+    double b = console.nextDouble();
+
+    double hipotenusa = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    System.out.format("A hipotenusa é: %.2f", hipotenusa);
+  }
+}
